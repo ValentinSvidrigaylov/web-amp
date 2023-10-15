a26fd887cf4caa4cc21846561b9dea7cb5f526d1
ValentinSvidrigaylov <sergeyshe07@gmail.com>
2023-10-15 13:51:59 +0300

    rebuild app

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 
+const repo = 'web-amp'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
+
 const nextConfig = {
-    output: 'export',
+    assetPrefix: assetPrefix,
+    basePath: basePath,
+    output: 'export'
 }
 
 module.exports = nextConfig
